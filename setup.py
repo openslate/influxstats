e68f4848862a939f2a43a99dd74fccd8b97517d4
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2017-10-10 15:03:15 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import find_packages, setup
+
+packages = find_packages('src')
+print(packages)
+
+setup(
+    name='influxstats',
+    url='https://github.com/openslate/influxstats',
+    author='Openslate',
+    author_email='code@osslabs.net',
+    version='0.0.4',
+    packages=packages,  # include all packages under src
+    package_dir={'':'src'},   # tell distutils packages are under src
+
+    install_requires=[
+        'statsd',
+    ]
+)
