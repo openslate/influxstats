e1e8be92e9d78d4e12ad3ee09d191fd01282b7ac
Roberto    Aguilar <roberto.c.aguilar@gmail.com>
2018-10-24 14:46:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,22 @@ import os
 from setuptools import find_packages, setup
 
 packages = find_packages('src')
-print(packages)
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
 
 setup(
     name='influxstats',
     url='https://github.com/openslate/influxstats',
     author='Openslate',
-    author_email='code@osslabs.net',
+    author_email='code@openslate.com',
+    description='Create consistent stats across your entire codebase',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
     version='0.0.0',
     packages=packages,  # include all packages under src
     package_dir={'':'src'},   # tell distutils packages are under src
-
     install_requires=[
         'statsd',
     ]
